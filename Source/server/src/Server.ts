f555a682a52e188e044f8d8f6d181b600892b7ff
viet-cuong <viet-cuong@system-exe.com.vn>
2023-11-20 14:47:31 +0700

    hot fix cors

--- Source/server/src/Server.ts
+++ Source/server/src/Server.ts
@@ -1,4 +1,4 @@
-import cors from "cors";
+import cors, { CorsOptions } from "cors";
 import express, { Application } from "express";
 import authRoutes from "./auth/auth.routes";
 import DBConnector from "./db/DBConnector";
@@ -16,7 +16,14 @@ export default class Server {
     }
 
     private configServer(app: Application) {
-        app.use(cors());
+        const CORS_OPTIONS: CorsOptions = {
+            origin: '*',
+            methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+            preflightContinue: false,
+            optionsSuccessStatus: 204,
+        }
+
+        app.use(cors(CORS_OPTIONS));
         app.use(express.json());
         app.use(express.urlencoded({ extended: true }));
     }
