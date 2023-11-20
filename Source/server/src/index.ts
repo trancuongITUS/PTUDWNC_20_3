8d91cc5e351ecb0aa4427d9371aecebe8eed71a4
viet-cuong <viet-cuong@system-exe.com.vn>
2023-11-20 14:35:10 +0700

    Hot fix cor

--- Source/server/src/index.ts
+++ Source/server/src/index.ts
@@ -1,9 +1,11 @@
+import cors from "cors";
 import express, {Application, Request, Response} from 'express';
 import dotenv from 'dotenv';
 import Server from './Server';
 
 dotenv.config();
 const APP: Application = express();
+APP.use(cors())
 const SERVER: Server = new Server(APP);
 const PORT = process.env.PORT ? parseInt(process.env.PORT as string, 10) : 8080;
 
