9c45fdd0166896c0c68a94581d5597c650a90e4b
minh-tri <cmtri2601@gmail.com>
2024-01-12 02:40:22 +0700

    update docker file

--- Source/server/src/db/DBConnector.ts
+++ Source/server/src/db/DBConnector.ts
@@ -1,33 +1,34 @@
-import { Sequelize, Utils } from "sequelize";
-import Util from "~/utils/Util";
+import { Sequelize, Utils } from 'sequelize'
+import Util from '~/utils/Util'
 
 export default class DBConnector {
-    private static instance: DBConnector;
-    private sequelize: Sequelize;
+  private static instance: DBConnector
+  private sequelize: Sequelize
 
-    private constructor() {
-        this.sequelize = new Sequelize('ptudwnc', 'postgres', '123456?a', {
-            host: 'localhost',
-            dialect: 'postgres',
-            
-            pool: {
-                max: 5,
-                min: 0,
-                acquire: 30000,
-                idle: 10000,
-            }
-        });
+  private constructor() {
+    this.sequelize = new Sequelize('postgres', 'postgres', 'postgres', {
+      host: 'postgres-aws.ch4eqas8ow8j.ap-southeast-1.rds.amazonaws.com',
+      dialect: 'postgres',
+      // native: false,
+
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      }
+    })
+  }
+
+  public getConnector(): Sequelize {
+    return this.sequelize
+  }
+
+  public static getInstance(): DBConnector {
+    if (Util.isNullOrUndefined(this.instance)) {
+      this.instance = new DBConnector()
     }
 
-    public getConnector(): Sequelize {
-        return this.sequelize;
-    }
-
-    public static getInstance(): DBConnector {
-        if (Util.isNullOrUndefined(this.instance)) {
-            this.instance = new DBConnector();
-        }
-
-        return this.instance;
-    }
+    return this.instance
+  }
 }
