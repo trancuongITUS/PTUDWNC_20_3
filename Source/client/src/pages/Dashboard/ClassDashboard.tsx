4619453d3c230dda25929c3fed82fb76fa0ff9df
minh-tri <cmtri2601@gmail.com>
2024-01-15 19:16:52 +0700

    demo

--- Source/client/src/pages/Dashboard/ClassDashboard.tsx
+++ Source/client/src/pages/Dashboard/ClassDashboard.tsx
@@ -220,7 +220,7 @@ const ClassDashboard = () => {
                                 className="mb-3 block text-sm font-medium text-black dark:text-white"
                                 htmlFor="invitationCode"
                             >
-                                Class cod
+                                Class code
                             </label>
                             <input
                                 className="w-full rounded border border-stroke bg-gray py-3 px-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
